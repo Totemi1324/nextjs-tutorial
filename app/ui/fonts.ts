029f19492c202778923ea2745e571b47bde6e964
Tamas Nemes <nemestomi2@gmail.com>
2024-03-16 23:19:36 +0100

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Roboto_Slab, Jaldi } from 'next/font/google';
+ 
+export const primary_font = Jaldi({
+    subsets: ['latin'],
+    weight: "400"
+});
+export const secondary_font = Roboto_Slab({ subsets: ['latin'] });
